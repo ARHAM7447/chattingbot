dfd1730d0ae65a5d83316da9379496d96f4bdfce
ARHAM7447 <153376544+ARHAM7447@users.noreply.github.com>
2025-02-16 22:07:36 +0530

    add the code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tools.tool1.routes import tool1_bp  # Import tool1 blueprint
 from tools.tool2.routes1 import tool2_bp  # Import tool2 blueprint
 from tools.tool3.routsee import tool3_bp
 from tools.tool4.routing import tool4_bp  # Import tool4 blueprint
+
 app = Flask(__name__, template_folder='templates')
 
 # Register Blueprints for all AI tools
@@ -10,7 +11,6 @@ app.register_blueprint(tool1_bp, url_prefix='/tool1')  # Use url_prefix for clar
 app.register_blueprint(tool2_bp, url_prefix='/tool2')  # Register tool2 properly
 app.register_blueprint(tool3_bp, url_prefix='/tool3')  # Register tool3 properly
 app.register_blueprint(tool4_bp, url_prefix='/tool4')  # Register tool4 properly
-
 
 @app.route('/')
 @app.route('/index')
